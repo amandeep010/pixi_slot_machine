a15009b4124d8d92c1054c348078cc9c8652df77
Aman <7618deep@gmail.com>
2025-04-19 12:44:59 +0530

    reel different stop and updates

--- src/RotateSlots.ts
+++ src/RotateSlots.ts
@@ -1,5 +1,8 @@
+import { Sprite } from 'pixi.js'
 import app from './app'
+import { frameBottom } from './helper/Helper'
 
+//shuffle cards
 const shuffle = (array: string[]) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
@@ -8,27 +11,45 @@ const shuffle = (array: string[]) => {
   return array
 }
 
-export const RotateSlots = async (spinWheel: any, spriteSheet: any[]) => {
-  spinWheel.interactive = false
-  const reelHeight = window.innerHeight / 2 - 100 // Total height of all sprites in a reel (4 sprites * 145px each)
-  const spinDuration = 3000
-  const startTime = Date.now()
-  const frameBottom = window.innerHeight / 2 + 550 // Frame's bottom boundary
-
-  // Group sprites into reels
+//Creating reels
+const createReel = (spriteSheet: Sprite[], initialPosition: number) => {
   let reels: any[] = []
-  for (let i = 0; i < 6; i++) {
+  for (let i = initialPosition; i < 6; i++) {
     reels.push(spriteSheet.slice(i * 5, (i + 1) * 5))
   }
+  return reels
+}
+
+//Set Reels position
+const setReelPosition = (reels: Sprite[][]) => {
+  reels.forEach((reel) => {
+    reel.forEach((sprite: any, spriteIndex: number) => {
+      sprite.y = window.innerHeight / 2 - 500 + spriteIndex * 250
+    })
+  })
+}
+
+export const RotateSlots = async (spinWheel: Sprite, spriteSheet: Sprite[]) => {
+  spinWheel.interactive = false
+  const reelHeight = window.innerHeight / 2 - 100 // Total height of all sprites in a reel (4 sprites * 145px each)
+  const spinDuration = 4000
+  const startTime = Date.now()
+  let data = 0
+
+  // Group sprites into reels
+  let reels = createReel(spriteSheet, 0)
 
   const animateSpin = () => {
+    data += 1
     const elapsed = Date.now() - startTime
+    console.log('data', data)
     const progress = Math.min(elapsed / spinDuration, 1)
+    let choosenColumn = reels
 
-    const animate = () => {
-      reels.forEach((reel, reelIndex) => {
+    const animate = (reelData: Sprite[]) => {
+      reelData.forEach((reel: any, reelIndex: number) => {
         reels[reelIndex] = shuffle(reel)
-        reels[reelIndex].forEach((sprite: any) => {
+        reel.forEach((sprite: any) => {
           sprite.y += 100
           if (sprite.y > frameBottom) {
             sprite.y -= reelHeight
@@ -38,14 +59,38 @@ export const RotateSlots = async (spinWheel: any, spriteSheet: any[]) => {
     }
 
     if (progress < 1) {
-      requestAnimationFrame(animate)
+      const reel: number = 0
+      switch (data) {
+        case 40:
+          choosenColumn = createReel(spriteSheet, reel + 1)
+          setReelPosition(reels)
+          requestAnimationFrame(() => animate(choosenColumn))
+          break
+        case 80:
+          choosenColumn = createReel(spriteSheet, reel + 2)
+          setReelPosition(reels)
+          requestAnimationFrame(() => animate(choosenColumn))
+          break
+        case 120:
+          choosenColumn = createReel(spriteSheet, reel + 3)
+          setReelPosition(reels)
+          requestAnimationFrame(() => animate(choosenColumn))
+          break
+        case 160:
+          choosenColumn = createReel(spriteSheet, reel + 4)
+          setReelPosition(reels)
+          requestAnimationFrame(() => animate(choosenColumn))
+          break
+        case 200:
+          choosenColumn = createReel(spriteSheet, reel + 5)
+          setReelPosition(reels)
+          requestAnimationFrame(() => animate(choosenColumn))
+          break
+        default:
+          requestAnimationFrame(() => animate(choosenColumn))
+      }
     } else {
-      reels.forEach((reel) => {
-        reel.forEach((sprite: any, spriteIndex: number) => {
-          sprite.y = window.innerHeight / 2 - 500 + spriteIndex * 250
-          frameBottom - 145
-        })
-      })
+      setReelPosition(reels)
       spinWheel.interactive = true
       app.ticker.remove(animateSpin)
       return reels
--- src/helper/Helper.ts
+++ src/helper/Helper.ts
@@ -1,68 +1,69 @@
-import { TextStyle } from "pixi.js";
+import { TextStyle } from 'pixi.js'
 
 //static variables are includede here.
 export const reels = [
-      ["A", "B", "Z", "D", "A"],
-      ["B", "Z", "Y", "D", "D"],
-      ["Y", "D", "B", "Z", "B"],
-      ["Z", "A", "D", "Y", "A"],
-      ["Z", "A", "D", "Y", "A"],
-      ["Z", "A", "D", "Y", "A"] 
-];
+  ['A', 'B', 'Z', 'D', 'A'],
+  ['B', 'Z', 'Y', 'D', 'D'],
+  ['Y', 'D', 'B', 'Z', 'B'],
+  ['Z', 'A', 'D', 'Y', 'A'],
+  ['Z', 'A', 'D', 'Y', 'A'],
+  ['Z', 'A', 'D', 'Y', 'A']
+]
 
-export const lowCard = ["A", "B", "C", "D", "E"];
-export const highCard = ["W", "X", "Y", "Z"];
+export const frameBottom = window.innerHeight / 2 + 550 // Frame's bottom boundary
+
+export const lowCard = ['A', 'B', 'C', 'D', 'E']
+export const highCard = ['W', 'X', 'Y', 'Z']
 
 export const style = new TextStyle({
-      fill: "#ffffff",
-      fillGradientType: 1,
-      fontFamily: "Courier New",
-      fontSize: 150,
-      //@ts-ignore
-      fontWeight: 600,
-      miterLimit: 33,
-      //@ts-ignore
-      padding: "",
-      stroke: "#608de6",
-      strokeThickness: 28,
-});
+  fill: '#ffffff',
+  fillGradientType: 1,
+  fontFamily: 'Courier New',
+  fontSize: 150,
+  //@ts-ignore
+  fontWeight: 600,
+  miterLimit: 33,
+  //@ts-ignore
+  padding: '',
+  stroke: '#608de6',
+  strokeThickness: 28
+})
 
 export const betStyle = new TextStyle({
-    fill: "#ffffff",
-    fillGradientType: 1,
-    fontFamily: "Tahoma",
-    fontSize: 50,
-    //@ts-ignore
-    fontWeight: 300,
-    miterLimit: 33,
-    //@ts-ignore
-    padding: "",
-    stroke: "#608de6"
-});
+  fill: '#ffffff',
+  fillGradientType: 1,
+  fontFamily: 'Tahoma',
+  fontSize: 50,
+  //@ts-ignore
+  fontWeight: 300,
+  miterLimit: 33,
+  //@ts-ignore
+  padding: '',
+  stroke: '#608de6'
+})
 
 export const winStyle = new TextStyle({
-    fill: "#ecbb09",
-    fillGradientType: 1,
-    fontFamily: "Comic Sans MS",
-    fontSize: 62,
-    //@ts-ignore
-    fontWeight: 600,
-    miterLimit: 33,
-    //@ts-ignore
-    padding: "",
-    stroke: "#608de6"
-});
+  fill: '#ecbb09',
+  fillGradientType: 1,
+  fontFamily: 'Comic Sans MS',
+  fontSize: 62,
+  //@ts-ignore
+  fontWeight: 600,
+  miterLimit: 33,
+  //@ts-ignore
+  padding: '',
+  stroke: '#608de6'
+})
 
 export const winAmountStyle = new TextStyle({
-    fill: "#ffffff",
-    fillGradientType: 1,
-    fontFamily: "Comic Sans MS",
-    fontSize: 62,
-    // @ts-ignore
-    fontWeight: 600,
-    miterLimit: 33,
-    // @ts-ignore
-    padding: "",
-    stroke: "#608de6"
-});
-
+  fill: '#ffffff',
+  fillGradientType: 1,
+  fontFamily: 'Comic Sans MS',
+  fontSize: 62,
+  // @ts-ignore
+  fontWeight: 600,
+  miterLimit: 33,
+  // @ts-ignore
+  padding: '',
+  stroke: '#608de6'
+})
